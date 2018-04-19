eb8d0b938b1ae6d0d18e08b8f8b2729048e96bb3
RMosley <ronnie.j.mosley@gmail.com>
2018-04-18 22:16:23 -0700

    Urls updated 4/18/2018

--- django_files/musicapp/pages/views.py
+++ django_files/musicapp/pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def products(request):
+    return render(request, 'pages/products.html')
+
+def store(request):
+    return render(request, 'pages/store.html')
