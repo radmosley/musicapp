fa7f6232692285ec7a5789a409a2c3bca7ff703d
RMosley <ronnie.j.mosley@gmail.com>
2018-04-20 21:38:13 -0700

    Update 4/20/2018

--- django_files/musicapp/pages/urls.py
+++ django_files/musicapp/pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('products/', views.products, name='products'),
+    path('store/', views.store, name='store'),
 ]
